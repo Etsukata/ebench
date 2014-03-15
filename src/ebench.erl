d9a65b69b25719e1d5ef1ad5c3e6ba9421273a39
Eiichi Tsukata <devel@etsukata.com>
2014-03-16 08:46:12 +0900

    add edoc description

--- src/ebench.erl
+++ src/ebench.erl
@@ -1,13 +1,33 @@
+%%
+%% @author Eiichi Tsukata <devel@etsukata.com>
+%% @doc ebench - Erlang Benchmark Suite
+%% @copyright 2014 Eiichi Tsukata
+%%
+
+
 -module(ebench).
 -compile([debug_info]).
--export([start/2, ring/2, ping_pong/1]).
+-export([start/0, ring/2, ping_pong/1]).
 
-start(2,3) ->
+%% @doc start
+start() ->
     ring(2,3).
 
-ring(N, M) ->
-    ebench_ring:start(N, M).
-
+-spec ping_pong(N :: integer()) -> ok.
+%%
+%% @doc Pass messages between two processes N times.
+%%
 ping_pong(N) ->
     ebench_ping_pong:start(N).
+
+-spec ring(N :: integer(), M :: integer()) -> ok.
+%%
+%% @doc Create N processes in a ring. Send a message round the ring M times.
+%% So that a total of N * M messages get sent.
+%% This is an excercise in the Erlang book
+%% <a href="http://pragprog.com/book/jaerlang/programming-erlang">
+%% Programming Erlang</a> writen by Joe Armstrong.
+%%
+ring(N, M) ->
+    ebench_ring:start(N, M).
 
